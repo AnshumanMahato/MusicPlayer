f4398dbd851a2df3b053d7370490549f9a9596a3
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-09-17 13:08:10 +0530

    app.json updated

--- plugins/inline.py
+++ plugins/inline.py
@@ -38,6 +38,16 @@ buttons = [
 @Client.on_inline_query()
 async def search(client, query):
     answers = []
+    if query.query == "ORU_MANDAN_PM_VANNU":
+        answers.append(
+            InlineQueryResultArticle(
+                title="Deploy",
+                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/MusicPlayer) below.</b>", disable_web_page_preview=True),
+                reply_markup=InlineKeyboardMarkup(buttons)
+                )
+            )
+        await query.answer(results=answers, cache_time=0)
+        return
     string = query.query.lower().strip().rstrip()
     if string == "":
         await client.answer_inline_query(
