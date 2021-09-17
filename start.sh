f436ce58731bd7c16d2c927bc820681348302b27
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-09-17 14:46:17 +0530

    start.sh changed

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/AnshumanMahato/MusicPlayer.git /MusicPlayer
 cd /MusicPlayer
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot..."
 python3 main.py
